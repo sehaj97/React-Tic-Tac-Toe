c6cfda52c352a3cb6891f9dccb4829095e771274
Sehajpreet Singh <sehajmagan@gmail.com>
2024-03-07 19:51:31 -0500

    added input for player names

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import Player from "./Components/Player"
 
 function App() {
 
 
   return (
-    <p>Coming Soon</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol='X' />
+          <Player name="Player 2" symbol='O' />
+        </ol>
+        GAME BOARD
+      </div>
+      LOGS
+    </main>
   )
 }
 
---
+++ src/Components/Player.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(name);
+    const handleEditClick = () => { setIsEditing(!isEditing) }
+    return (
+        <li >
+            <span className="player">
+                {(!isEditing) ? (<span className="player-name">{playerName}</span>) : (<input type="text" value={playerName} onChange={(e) => (setPlayerName(e.target.value))} required />)}
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+    );
+}
